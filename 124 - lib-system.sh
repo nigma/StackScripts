3ce586d2098939515dc2b82342561bcd1d332b01
Filip Wasilewski <en@ig.ma>
2012-05-15 19:06:06 +0200

    Added python-software-properties to system utils. Removed references to existing packages.

--- 124 - lib-system.sh
+++ 124 - lib-system.sh
@@ -7,7 +7,7 @@
 # My ref: http://www.linode.com/?r=aadfce9845055011e00f0c6c9a5c01158c452deb
 
 function system_install_utils {
-    aptitude -y install wget less vim sudo screen htop bsd-mailx
+    aptitude -y install htop bsd-mailx python-software-properties
 }
 
 function system_install_build {
