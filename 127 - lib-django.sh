94c35dea72fcf6736856e044334da3165b441b74
Filip Wasilewski <en@ig.ma>
2012-05-17 15:34:26 +0200

    Parametrized paths and removed unnecessary functions.

--- 127 - lib-django.sh
+++ 127 - lib-django.sh
@@ -6,61 +6,49 @@
 #
 # My ref: http://www.linode.com/?r=aadfce9845055011e00f0c6c9a5c01158c452deb
 
-
-function django_get_user_or_default {
-    # django_get_user_or_default([user])
-    if [ -z "$1" ];
-		then USER="www-data"
-		else USER="$1"
-	fi
-    echo "$USER"
-}
-
-function django_get_project_path {
-    # django_get_project_path(project_name)
-    PROJECT_NAME="$1"
-    echo "/srv/$PROJECT_NAME"
-}
+PROJECT_CODE_DIR=app
+DJANGO_PROJECT=webapp
 
 function django_change_project_owner {
     # django_change_project_owner(project_path, user)
     PROJECT_PATH="$1"
     USER="$2"
-    GROUP="$USER"
-    chown -R "$USER:$GROUP" "$PROJECT_PATH"
+    chown -R "$USER:$USER" "$PROJECT_PATH"
 }
 
 function django_create_project {
-    # django_create_project(project_name)
+    # django_create_project(project_path)
 
-    PROJECT_NAME="$1"
-    if [ -z "$PROJECT_NAME" ]; then
-        echo "django_create_project() requires the project name as the first argument"
+    PROJECT_PATH="$1"
+    if [ -z "$PROJECT_PATH" ]; then
+        echo "django_create_project() requires the project root path as the first argument"
         return 1;
     fi
-    PROJECT_PATH=`django_get_project_path "$PROJECT_NAME"`
 
-    mkdir -p "$PROJECT_PATH/app/conf/apache"
+    mkdir -p "$PROJECT_PATH/$PROJECT_CODE_DIR/conf/apache"
     mkdir -p "$PROJECT_PATH/logs" "$PROJECT_PATH/run/eggs"
 
     virtualenv "$PROJECT_PATH/venv"
-    $PROJECT_PATH/venv/bin/pip install django
+    $PROJECT_PATH/venv/bin/pip install Django
 
-    pushd "$PROJECT_PATH/app"
-    "$PROJECT_PATH/venv/bin/python" "$PROJECT_PATH/venv/bin/django-admin.py" startproject webapp .
+    pushd "$PROJECT_PATH/$PROJECT_CODE_DIR"
+    "$PROJECT_PATH/venv/bin/python" "$PROJECT_PATH/venv/bin/django-admin.py" startproject "$DJANGO_PROJECT" .
     popd
-    mkdir -p "$PROJECT_PATH/app/webapp/static"
+    mkdir -p "$PROJECT_PATH/$PROJECT_CODE_DIR/$DJANGO_PROJECT/static"
+
+    echo "Django" >> "$PROJECT_PATH/$PROJECT_CODE_DIR/requirements.txt"
 }
 
 function django_install_db_driver {
     # django_install_db_driver(project_path, driver_package)
-    $1/venv/bin/pip install django "$2"
+    $1/venv/bin/pip install "$2"
+    echo "$2" >> "$PROJECT_PATH/$PROJECT_CODE_DIR/requirements.txt"
 }
 
 function django_configure_db_settings {
     # django_configure_db_settings(project_path, engine, name, user, password, [host, [port]])
-
-    SETTINGS="$1/app/webapp/settings.py"
+    PROJECT_PATH="$1"
+    SETTINGS="$PROJECT_PATH/$PROJECT_CODE_DIR/$DJANGO_PROJECT/settings.py"
     sed -i -e "s/'ENGINE': 'django.db.backends.'/'ENGINE': 'django.db.backends.$2'/" "$SETTINGS"
     sed -i -e "s/'NAME': ''/'NAME': '$3'/" "$SETTINGS"
     sed -i -e "s/'USER': ''/'USER': '$4'/" "$SETTINGS"
@@ -79,7 +67,7 @@ function django_configure_apache_virtualhost {
     VHOST_HOSTNAME="$1"
     PROJECT_PATH="$2"
     USER="$3"
-	GROUP="$USER"
+    GROUP="$USER"
 
     if [ -z "$VHOST_HOSTNAME" ]; then
         echo "django_configure_apache_virtualhost() requires the hostname as the first argument"
@@ -92,7 +80,7 @@ function django_configure_apache_virtualhost {
     fi
 
     APACHE_CONF="200-$VHOST_HOSTNAME"
-    APACHE_CONF_PATH="$PROJECT_PATH/app/conf/apache/$APACHE_CONF"
+    APACHE_CONF_PATH="$PROJECT_PATH/$PROJECT_CODE_DIR/conf/apache/$APACHE_CONF"
 
     cat > "$APACHE_CONF_PATH" << EOF
 <VirtualHost *:80>
@@ -100,28 +88,28 @@ function django_configure_apache_virtualhost {
     ServerName $VHOST_HOSTNAME
     ServerSignature Off
 
-    Alias /static/ $PROJECT_PATH/app/webapp/static/
-    Alias /robots.txt $PROJECT_PATH/app/webapp/static/robots.txt
-    Alias /favicon.ico $PROJECT_PATH/app/webapp/static/favicon.ico
+    Alias /static/ $PROJECT_PATH/$PROJECT_CODE_DIR/$DJANGO_PROJECT/static/
+    Alias /robots.txt $PROJECT_PATH/$PROJECT_CODE_DIR/$DJANGO_PROJECT/static/robots.txt
+    Alias /favicon.ico $PROJECT_PATH/$PROJECT_CODE_DIR/$DJANGO_PROJECT/static/favicon.ico
 
     SetEnvIf User_Agent "monit/*" dontlog
     CustomLog "|/usr/sbin/rotatelogs $PROJECT_PATH/logs/access.log.%Y%m%d-%H%M 5M" combined env=!dontlog
     ErrorLog "|/usr/sbin/rotatelogs $PROJECT_PATH/logs/error.log.%Y%m%d-%H%M 5M"
     LogLevel warn
 
-    WSGIScriptAlias / $PROJECT_PATH/app/webapp/wsgi.py
+    WSGIScriptAlias / $PROJECT_PATH/$PROJECT_CODE_DIR/$DJANGO_PROJECT/wsgi.py
 
-    WSGIDaemonProcess $VHOST_HOSTNAME user=$USER group=$GROUP processes=2 threads=10 maximum-requests=10000 display-name=%{GROUP} python-path=$PROJECT_PATH/app:$PROJECT_PATH/venv/lib/python2.7/site-packages python-eggs=$PROJECT_PATH/run/eggs
+    WSGIDaemonProcess $VHOST_HOSTNAME user=$USER group=$GROUP processes=2 threads=10 maximum-requests=10000 display-name=%{GROUP} python-path=$PROJECT_PATH/$PROJECT_CODE_DIR:$PROJECT_PATH/venv/lib/python2.7/site-packages python-eggs=$PROJECT_PATH/run/eggs
     WSGIProcessGroup $VHOST_HOSTNAME
-    WSGIScriptAlias / $PROJECT_PATH/app/webapp/wsgi.py
+    WSGIScriptAlias / $PROJECT_PATH/$PROJECT_CODE_DIR/$DJANGO_PROJECT/wsgi.py
 
-    <Directory $PROJECT_PATH/app/webapp/static>
+    <Directory $PROJECT_PATH/$PROJECT_CODE_DIR/$DJANGO_PROJECT/static>
         Order deny,allow
         Allow from all
         Options -Indexes FollowSymLinks
     </Directory>
 
-    <Directory $PROJECT_PATH/app/conf/apache>
+    <Directory $PROJECT_PATH/$PROJECT_CODE_DIR/conf/apache>
         Order deny,allow
         Allow from all
     </Directory>
@@ -129,6 +117,6 @@ function django_configure_apache_virtualhost {
  </VirtualHost>
 EOF
 
-    ln -t /etc/apache2/sites-available/ "$APACHE_CONF_PATH"
+    ln -s -t /etc/apache2/sites-available/ "$APACHE_CONF_PATH"
     a2ensite "$APACHE_CONF"
 }
